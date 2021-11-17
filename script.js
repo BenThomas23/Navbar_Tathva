06fac82c2dcba27511289a1b0111543115d5d1b3
Ben Thomas <75978175+BenThomas23@users.noreply.github.com>
2021-11-17 15:34:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ hamburger.addEventListener("click", () =>{
     cont.classList.toggle("open1");
     hamburger.classList.toggle("open2");
     if(cont.classList.value=="cont open1"){
-        gsap.from(".nav-links li a", {duration:1.5,opacity:0,delay:0.5})
+        gsap.from(".nav-links li a", {duration:1.5,opacity:0,delay:0.6,stagger:0.3})
     }
 }); 
-
-
